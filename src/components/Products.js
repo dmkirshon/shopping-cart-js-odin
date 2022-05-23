aac7dee2095c60df769df0024c0aa8f1ec9b2293
dmkirshon <kirshon.david@gmail.com>
2022-05-23 16:07:14 +0200

    feat: Setup intial API shopping call with img

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,18 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Products = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const productsFetch = await fetch("https://fakestoreapi.com/products");
+      const productsData = await productsFetch.json();
+
+      setProducts(productsData);
+      console.log(productsData);
+    };
+    fetchProducts().catch(console.error);
+  }, []);
+
+  const displayProducts = () => {
+    return products.map((productInfo) => {
+      return (
+        <div key={productInfo.id}>
+          <img className="products-image" src={productInfo.image} />
+          <p>{productInfo.title}</p>
+          <p>{productInfo.rating.rate} ⭐️</p>
+        </div>
+      );
+    });
+  };
+
   return (
     <div className="products">
       <h1>Products</h1>
-      <div className="products-main">
-        <div src="" className="products-image" />
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet,
-          cupiditate maxime. Qui impedit inventore, voluptatibus quidem nihil
-          iste maxime, eveniet possimus, perferendis est veritatis adipisci
-          repellat reiciendis. Atque, exercitationem nihil.
-        </p>
-      </div>
+      <div className="products-main">{displayProducts()}</div>
     </div>
   );
 };
