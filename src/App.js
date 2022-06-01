9f3c7fa91c7c40351b5aa0b91a999f8c4cf4955c
dmkirshon <kirshon.david@gmail.com>
2022-06-01 16:49:30 +0200

    chores: Add basename for routing on github

--- src/App.js
+++ src/App.js
@@ -65,7 +65,7 @@ function App() {
 
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename="/">
         <NavBar
           cartSize={shoppingCart.reduce(
             (totalProducts, product) => totalProducts + product.count,
